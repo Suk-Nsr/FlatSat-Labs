1e59902269a12c1dea38037708dbe6a1af353b75
Kittiphong <kittiphongss63@gmail.com>
2026-06-10 16:35:06 +0700

    change to serial input and clean lab 3.2

--- Lab_3/Solution/Lab3_Power_Control_Solution/Lab3_Power_Control_Solution.ino
+++ Lab_3/Solution/Lab3_Power_Control_Solution/Lab3_Power_Control_Solution.ino
@@ -1,8 +1,7 @@
 
 // NBSPACE Labs: FlatSat Learning Set
-// Lab 3.2: OBC Power Control (Power Lines Manipulation)
+// Lab 3.2: OBC Power Control (Interactive Serial Control)
 // Solution Code
-
 
 #include <Arduino.h>
 
@@ -15,42 +14,81 @@ void setup() {
   Serial.setTx(PD8);
   Serial.begin(115200);
 
-  delay(4000)
+  delay(4000);
 
-  Serial.println("\n=== FlatSat Lab 3.2: Manual Power Control Booting ===");
+  Serial.println("\n=== FlatSat Lab 3.2: Interactive Power Control ===");
+  Serial.println("Type the following keys in the Serial Monitor and press Enter:");
+  Serial.println("  '1' = Payload 1 ON   |  'q' = Payload 1 OFF");
+  Serial.println("  '2' = Payload 2 ON   |  'w' = Payload 2 OFF");
+  Serial.println("  '3' = COMMS ON       |  'e' = COMMS OFF");
+  Serial.println("  '0' = Turn ALL OFF");
+  Serial.println("--------------------------------------------------");
 
-  pinMode(PIN_COMMS, OUTPUT);
   pinMode(PIN_PAYLOAD_1, OUTPUT);
   pinMode(PIN_PAYLOAD_2, OUTPUT);
+  pinMode(PIN_COMMS, OUTPUT);
 
-  digitalWrite(PIN_PAYLOAD_1, LOW);
-  digitalWrite(PIN_PAYLOAD_2, LOW);
-  digitalWrite(PIN_COMMS, LOW);
-  Serial.println("Initial State: All Subsystems OFF (0V)");
-  Serial.println("Waiting 3 seconds to begin sequence...\n");
-  delay(3000);
+  digitalWrite(PIN_PAYLOAD_1, HIGH);
+  digitalWrite(PIN_PAYLOAD_2, HIGH);
+  digitalWrite(PIN_COMMS, HIGH);
+  Serial.println("Initial State: All Subsystems ON\n");
 }
 
 void loop() {
-  Serial.println("--- Power Sequence Start ---");
+  // Check if any data has been typed into the Serial Monitor
+  if (Serial.available() > 0) {
+    char cmd = Serial.read(); // Read the first character
 
-  digitalWrite(PIN_PAYLOAD_1, HIGH);
-  Serial.println("Payload 1 (PD1) -> ON");
-  delay(4000);
+    // Match the character to the corresponding command
+    switch (cmd) {
+      // --- Payload 1 (PD2) ---
+      case '1':
+        digitalWrite(PIN_PAYLOAD_1, HIGH);
+        Serial.println("Payload 1 (PD2) -> ON");
+        break;
+      case 'q':
+        digitalWrite(PIN_PAYLOAD_1, LOW);
+        Serial.println("Payload 1 (PD2) -> OFF");
+        break;
 
-  digitalWrite(PIN_PAYLOAD_2, HIGH);
-  Serial.println("Payload 2 (PD3) -> ON");
-  delay(4000);
+      // --- Payload 2 (PD3) ---
+      case '2':
+        digitalWrite(PIN_PAYLOAD_2, HIGH);
+        Serial.println("Payload 2 (PD3) -> ON");
+        break;
+      case 'w':
+        digitalWrite(PIN_PAYLOAD_2, LOW);
+        Serial.println("Payload 2 (PD3) -> OFF");
+        break;
 
-  digitalWrite(PIN_COMMS, HIGH);
-  Serial.println("COMMS (PD2)     -> ON");
-  delay(4000);
+      // --- COMMS (PD1) ---
+      case '3':
+        digitalWrite(PIN_COMMS, HIGH);
+        Serial.println("COMMS (PD1)     -> ON");
+        break;
+      case 'e':
+        digitalWrite(PIN_COMMS, LOW);
+        Serial.println("COMMS (PD1)     -> OFF");
+        break;
 
-  digitalWrite(PIN_PAYLOAD_1, LOW);
-  digitalWrite(PIN_PAYLOAD_2, LOW);
-  digitalWrite(PIN_COMMS, LOW);
-  Serial.println("All Subsystems  -> OFF");
-  Serial.println("----------------------------\n");
-  
-  delay(5000);
+      // --- ALL OFF ---
+      case '0':
+        digitalWrite(PIN_PAYLOAD_1, LOW);
+        digitalWrite(PIN_PAYLOAD_2, LOW);
+        digitalWrite(PIN_COMMS, LOW);
+        Serial.println("All Subsystems  -> OFF");
+        break;
+
+      // Ignore standard invisible line-ending characters
+      case '\n':
+      case '\r':
+      case ' ':
+        break;
+
+      // Handle unrecognized inputs
+      default:
+        Serial.println("Unknown command. Use 1/q, 2/w, 3/e, or 0.");
+        break;
+    }
+  }
 }
