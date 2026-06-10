31a0a865d3127b4789503455d367dabc0d791190
Kittiphong <kittiphongss63@gmail.com>
2026-06-10 15:29:39 +0700

    change while !serial to delay

--- Lab_3/Solution/Lab3_Power_Control_Solution/Lab3_Power_Control_Solution.ino
+++ Lab_3/Solution/Lab3_Power_Control_Solution/Lab3_Power_Control_Solution.ino
@@ -14,7 +14,8 @@ void setup() {
   Serial.setRx(PD9);
   Serial.setTx(PD8);
   Serial.begin(115200);
-  while (!Serial) {;}
+
+  delay(4000)
 
   Serial.println("\n=== FlatSat Lab 3.2: Manual Power Control Booting ===");
 
